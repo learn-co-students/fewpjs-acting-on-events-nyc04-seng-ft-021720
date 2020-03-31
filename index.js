1ff6be04ed5a4299666dd04400c6a3b47abceec4
96StanleyLee <96stanleylee@gmail.com>
2020-03-31 16:33:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+let dodger = document.querySelector("#dodger")
+dodger.style.right = '179px'
+
+function moveDodgerLeft(){
+    let leftNumbers = dodger.style.left.replace('px', '')
+    let left = parseInt(leftNumbers, 10);
+    if (left > 0 ){
+        dodger.style.left = `${left-1}px`
+    }
+}
+
+
+
+function moveDodgerRight(){
+    let leftNumbers = dodger.style.left.replace('px', '')
+    let left = parseInt(leftNumbers, 10);
+    if (left > 0 ){
+        dodger.style.left = `${left+1}px`
+    }
+}
+
+
+
+
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    else if (e.key === "ArrowRight"){
+        moveDodgerRight()
+    }
+  });
