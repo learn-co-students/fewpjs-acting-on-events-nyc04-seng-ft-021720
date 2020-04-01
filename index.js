d791dc35f72ebaf20e200e30a96313d3993da483
erocknine <echeungsk@gmail.com>
2020-03-31 20:40:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left >= 1) {
+    dodger.style.left = `${left - 6}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left <= 359) {
+    dodger.style.left = `${left + 6}px`;
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
